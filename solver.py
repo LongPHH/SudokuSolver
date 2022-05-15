e623fb40c5e4d9a4068f1b03a705e1f02d991b01
Nilesh <nileshmukherji@gmail.com>
2022-05-15 17:11:13 -0400

    Nilesh is a genius and can solve all problems bc he is god with cs and long is his loyal follower

--- solver.py
+++ solver.py
@@ -1,10 +1,22 @@
-<<<<<<< HEAD
 # This sodoku solver relies on the assumptions that any inputs are valid problems (i.e. no dissallowed conditions within input)
 
-=======
-import numpy as np
-import sys
->>>>>>> 97992a571139993bb8152041fec545ffde60c29b
+from nbformat import write
+from numpy import insert
+from copy import deepcopy
+
+
+rows = {}
+for i in range(9):
+    rows[i] = [None] * 9
+
+cols = {}
+for i in range(9):
+    cols[i] = [None] * 9
+
+boxes = {}
+for i in range(3):
+    for j in range(3):
+        boxes[(i, j)] = [None]*9
 
 
 def getInput():     # get input from txt file and put into a 2d array
@@ -29,13 +41,14 @@ def getInput():     # get input from txt file and put into a 2d array
 
 
 # function to wrte to output file. solvedGrid = -1 if grid not solvable
-def writeOutput(solvedGrid):
+def writeOutput(solvedGridNode):
     file_name = input("Enter Output File name: ")
     file = open(file_name, 'w')
     
-    if solvedGrid == -1:
+    if solvedGridNode == -1:
         file.write("Not Solvable!\n")
     else:
+        solvedGrid = solvedGridNode.array
         for row in solvedGrid:
             for num in row:
                 file.write(str(num) + " ")
@@ -45,22 +58,85 @@ def writeOutput(solvedGrid):
 
 
 
-# sudoku constraints check
-model = np.array(range(1,10))           # model array, to use and check for constraints
-def check_row(board):           # check if row contain from 1-9 
-    for row in board:
-        if np.array_equal(np.unique(row), model) == False:
-            return False
-    return True
-
-
-
-
 
 # The choose_var function allows us to identify the best variable to next modify
 # The way it is implimented here checks for counts of values in the rows, cols and grids of all empty vars
 # if two values have same number of domain, use degree heuristic, if that also same, choose first one. 
 # if cannot choose var, means done.
+
+def parse_hash(arr):                # get all available domain 
+    for i in range(9):
+        rows[i] = [None] * 9
+    for i in range(9):
+        cols[i] = [None] * 9
+    for i in range(3):
+        for j in range(3):
+            boxes[(i, j)] = [None]*9
+    
+    for y in range(9):
+        for x in arr[y]:
+            if x != 0:
+                rows[y][x-1] = 1
+
+    for x in range(9):
+        for y in range(9):
+            if arr[y][x] != 0:
+                cols[x][arr[y][x] - 1] = 1
+
+    for adder_x in range(3):
+        for adder_y in range(3):
+            for x in range(3):
+                for y in range(3):
+                    if arr[3*adder_x + x][3*adder_y + y] != 0:
+                        boxes[(adder_y, adder_x)][arr[3*adder_x + x][3*adder_y + y] - 1] = 1
+
+
+def mrv(array):
+    available_vals = [None] * 9
+    mrv = [20, [(-1, -1), -1]]
+    for y in range(9):
+        for x in range(9):
+            if array[y][x] == 0:
+                deg_huer = 0
+                for i in range(len(cols[x])):
+                    if cols[x][i] != None:
+                        available_vals[i] = 1
+                    else:
+                        deg_huer += 1
+                for i in range(9):
+                    if rows[y][i] != None:
+                        available_vals[i] = 1
+                    else:
+                        deg_huer += 1
+                for i in range(9):
+                    if boxes[(y//3, x//3)][i] == None:
+                        available_vals[i] = 1
+                    else:
+                        deg_huer += 1
+                
+                remaining_vals = 0
+                for i in available_vals:
+                    if i==None:
+                        remaining_vals += 1
+                
+                if remaining_vals < mrv[0]:
+                    mrv = [remaining_vals, [(y, x), deg_huer]]
+                elif remaining_vals == mrv[0]:
+                    mrv.append([(y, x), deg_huer])
+                else: pass
+
+    if len(mrv) > 2:
+        mrv.pop(0)
+        deg = 0
+        coord = (0, 0)
+        for value in mrv:
+            if value[1] > deg:
+                coord = value[0]
+    else:
+        coord = mrv[1][0]
+    return coord
+
+
 
 def choose_var(array):      # array is the big grid
     # This is to check if the solution is complete
@@ -125,6 +201,11 @@ def choose_var(array):      # array is the big grid
     # Return MRV Value in tuple format, (Y, X)
     return val[1]
 
+def deg_huer(arr, lst):
+    for i in lst:
+        (Y, X) = lst[1]
+        
+
 
 # Simple node class for tree structure
 class Node:
@@ -138,12 +219,12 @@ def check_validity(array, num, loc):
     for x in array[loc[0]]:
         if x == num:
             return False
-    for y in range(10):
+    for y in range(9):
         if array[y][loc[1]] == num:
             return False
 
-    grid_x = loc[1] - x%3
-    grid_y = loc[0] - y%3
+    grid_x = loc[1] - loc[1]%3
+    grid_y = loc[0] - loc[0]%3
     for j in range(0, 3):
         for i in range(0, 3):
             if array[grid_y + j][grid_x + i] == num:
@@ -153,21 +234,39 @@ def check_validity(array, num, loc):
 
 # Main driver function. Modifies most relevant variables first, then builds a queue for a tree. This is closer to dfs than bfs. 
 queue = []
-def solve(node):
-    (Y, X) = choose_var(node.array)
+def solve(node, first):
+    if first != None:
+        queue.append(node)
+    parse_hash(node.array)
+    (Y, X) = mrv(node.array)
     if Y == -1 and X == -1:
         return node
+    queue.remove(node)
     for i in range (1, 10):
         if check_validity(node.array, i, (Y, X)):
-            new_arr = node.array
+            new_arr = deepcopy(node.array)
             new_arr[Y][X] = i
             queue.append(Node(node, new_arr))
-    solve(queue[-1])
+            print("Insertion: ", i, (Y, X))
+
+    
+    if queue == []:
+        return -1
+    else:
+        return (True, queue[-1])
 
 
 
 def main():
     grid = getInput()
-    val = choose_var(grid)
+    var = solve(Node(None,grid), 1)
+    count = 0
+    while type(var) == tuple:
+        print("Count: ", count)
+        print(var[1].array)
+        var = solve(var[1], None)
+        count += 1
+    print(var)
+    writeOutput(var)
 
 main()
